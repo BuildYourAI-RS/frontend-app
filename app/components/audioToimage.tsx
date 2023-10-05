4479a4bbbb1eb725322a018b63627bd2eb516bde
Rajat Agrawal <rajatagrawal072000@gmail.com>
2023-10-06 02:34:55 +0530

    added text to image feature

--- app/components/audioToimage.tsx
+++ app/components/audioToimage.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Upload from "./Upload";
+import Imagegen from "./Imagegen";
 import { useState } from "react";
 
 export default function AudioToImage() {
@@ -8,9 +9,10 @@ export default function AudioToImage() {
   const [transcript, setTranscript] = useState();
 
   return (
-    <>
+    <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="m-3">Upload Your File Here</h1>
       {/* <Upload setAudio={setAudio} setTranscript={setTranscript}></Upload> */}
-    </>
+      <Imagegen></Imagegen>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import AudioToImage from "./components/audioToimage";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col justify-center items-center h-screen">
         <AudioToImage></AudioToImage>
-      </div>
     </main>
   );
 }
