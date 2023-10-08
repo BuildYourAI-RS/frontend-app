b28b92f8d254175b6cf844bc98910c650261e9b1
Siddharth Gandhi <gandhi.siddharth01@gmail.com>
2023-10-09 01:38:14 +0530

    integrated wavesurfer.js

---
+++ app/components/Player.js
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+
+import styles from "./Player.module.css";
+
+const Player = ({ audio, audioWaveForm, dGTranscript }) => {
+  const [audioLength, setAudioLength] = useState("00:00");
+  const [currentTime, setCurrentTime] = useState("00:00");
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function toHHMMSS(secs) {
+    const sec_num = parseInt(secs, 10);
+    const hours = Math.floor(sec_num / 3600);
+    const minutes = Math.floor(sec_num / 60) % 60;
+    const seconds = sec_num % 60;
+
+    return [hours, minutes, seconds]
+      .map((v) => (v < 10 ? "0" + v : v))
+      .filter((v, i) => v !== "00" || i > 0)
+      .join(":");
+  }
+
+  useEffect(() => {
+    if (audioWaveForm) {
+      audioWaveForm.current.on("ready", function () {
+        setAudioLength(toHHMMSS(audioWaveForm.current.getDuration()));
+      });
+      audioWaveForm.current.on("finish", function () {
+        setIsPlaying(false);
+      });
+    }
+
+    const timer = setInterval(() => {
+      if (audioWaveForm) {
+        setAudioLength(toHHMMSS(audioWaveForm.current.getDuration()));
+        setCurrentTime(toHHMMSS(audioWaveForm.current.getCurrentTime()));
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [audioWaveForm]);
+
+  useEffect(() => {
+    setIsPlaying(false);
+  }, [audio]);
+
+  useEffect(() => {
+    if (dGTranscript) {
+      if (audioWaveForm) {
+        audioWaveForm.current.on("ready", function () {
+          setAudioLength(toHHMMSS(audioWaveForm.current.getDuration()));
+        });
+        audioWaveForm.current.on("finish", function () {
+          setIsPlaying(false);
+        });
+      }
+    }
+  }, [dGTranscript]);
+
+  const playAudio = () => {
+    if (audioWaveForm.current.isPlaying()) {
+      setIsPlaying(false);
+      audioWaveForm.current.pause();
+    } else {
+      setIsPlaying(true);
+      audioWaveForm.current.play();
+    }
+  };
+
+  return (
+    <div className=" grid w-auto items-center text-center">
+      <h2 className={styles.time}>
+        {currentTime}/{audioLength}
+      </h2>
+      <h3 className={styles.title}>
+        {audio ? audio.name : "LAB-LAB-AI-DEMO.mp3"}
+      </h3>
+      <div className={styles.player}>
+        {/* <img
+          className={styles.ff}
+          src="icons/fast-backward.png"
+          onClick={() => {
+            audioWaveForm.current.skipBackward(10);
+          }}
+          alt="ff-icon"
+        /> */}
+        <img
+          className={styles.play}
+          onClick={playAudio}
+          src={isPlaying ? "icons/pause.png" : "icons/play.png"}
+          alt="play-button"
+        />
+        {/* <img
+          className={styles.ff}
+          src="icons/fast-forward.png"
+          onClick={() => {
+            audioWaveForm.current.skipForward(10);
+
+            setAudioLength(toHHMMSS(audioWaveForm.current.getDuration()));
+
+            audioWaveForm.current.on("audioprocess", function () {
+              setCurrentTime(toHHMMSS(audioWaveForm.current.getCurrentTime()));
+            });
+          }}
+          alt="ff-icon"
+        /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Player;
---
+++ app/components/WaveForm.js
@@ -0,0 +1,62 @@
+import Wavesurfer from "wavesurfer.js";
+import { useState, useEffect, useRef } from "react";
+
+import styles from "./WaveForm.module.css";
+
+const WaveForm = ({ url, setAudioWaveForm }) => {
+  const waveform = useRef(null);
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    if (waveform.current) {
+      waveform.current.destroy();
+      waveform.current = null;
+    }
+
+    if (!waveform.current) {
+      waveform.current = Wavesurfer.create({
+        container: "#waveform",
+        waveColor: "white",
+        progressColor: "#0d76ff",
+        barGap: 5,
+        barWidth: 2,
+        barRadius: 2,
+        height: 120,
+        cursorWidth: 3,
+        cursorColor: "tomato",
+      });
+
+      if (typeof url == "string") {
+        console.log("url:",url)
+        waveform.current.load(url);
+      } else {
+        console.log("not url:",url)
+        waveform.current.loadBlob(url);
+      }
+
+
+      waveform.current.on("ready", function () {
+        setLoading(false);
+      });
+
+      waveform.current.on("loading", function () {
+        setLoading(true);
+      });
+
+      setAudioWaveForm(waveform);
+    }
+  }, [url]);
+
+  return (
+    <>
+      <div
+        id="waveform"
+        className={styles.waveform}
+        style={{ display: `${loading ? "hidden" : "block"}` }}
+      />
+      {loading && <h2>Loading waveform...</h2>}
+    </>
+  );
+};
+
+export default WaveForm;
