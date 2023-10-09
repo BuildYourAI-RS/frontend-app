d66f56a05608b1eca2d718b3c922a50010f88210
Siddharth Gandhi <gandhi.siddharth01@gmail.com>
2023-10-09 15:09:50 +0530

    update readme, dalle image gen

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+Generates image from audio files.
+
 ## Getting Started
 
-First, run the development server:
+First, install dependencies using npm:
+```bash
+npm install
+```
+Prepare a .env file for string API keys and insert this:
+Deepgram_API_Key = "Yoyr Deepgram API key"
+NEXT_PUBLIC_OPENAI_API_KEY = "Your OpenAI API key"
+NEXT_PUBLIC_STABILITY_API_KEY = "Your DreamStudio API key"
+
+Then, run the development server:
 ```bash
 npm run dev
 # or
@@ -14,8 +25,6 @@ pnpm dev
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
 ## Learn More
 
--- app/components/Imagegen.tsx
+++ app/components/Imagegen.tsx
@@ -10,8 +10,10 @@ const Imagegen = ({ dGTranscript }: { dGTranscript: string | undefined }) => {
   const [isTransSet, setTransSet] = useState(false);
   const [transcript, setTranscript] = useState("");
   const [images, setImages] = useState([]);
+  const [openaimg, setOpenaimg] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [imgPrompt, setImgPrompt] = useState<string>();
+  const [error, setError] = useState("ERROR GENERATING IMAGAES");
 
   const genPrompt = async (dGTranscript: string) => {
     const output =
@@ -41,13 +43,14 @@ const Imagegen = ({ dGTranscript }: { dGTranscript: string | undefined }) => {
         console.error(error.message); // e.g. The authentication token you passed was invalid...
         console.error(error.code); // e.g. 'invalid_api_key'
         console.error(error.type); // e.g. 'invalid_request_error'
+        setError(error.message);
         return;
       } else {
         // Non-API error
         console.log(error);
       }
     }
-    return response
+    return response;
   };
 
   useEffect(() => {
@@ -73,6 +76,7 @@ const Imagegen = ({ dGTranscript }: { dGTranscript: string | undefined }) => {
       const apiKey = process.env.NEXT_PUBLIC_STABILITY_API_KEY;
       if (!apiKey) {
         throw new Error("Missing Stability API key.");
+        setError("Missing Stability API key.");
       }
 
       const response = await fetch(
@@ -104,6 +108,37 @@ const Imagegen = ({ dGTranscript }: { dGTranscript: string | undefined }) => {
       setImages(generatedImages);
     } catch (error) {
       console.error("Error generating images:", error);
+      setError(String(error));
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const generateDalleImages = async () => {
+    setIsLoading(true);
+    if (!isTransSet) return;
+    const res = await genPrompt(transcript);
+    // console.log("res: ", res);
+    const imgPrompt = res!.choices[0]["text"];
+    // console.log("imgPromptUseEffect: ", imgPrompt);
+    // console.log("imgPromptButton: ", imgPrompt);
+    const prompt: string = JSON.parse(imgPrompt!).prompt;
+    // console.log("extracted prompt: ", prompt);
+    try {
+      const response = await openai.images.generate({
+        prompt,
+        n: 2,
+        size: "512x512",
+      });
+      // console.log(response.data)
+      console.log(response.data[0].url);
+      console.log(response.data[1].url);
+      const newUrlArray = [response.data[0].url!, response.data[1].url!];
+      setOpenaimg(newUrlArray);
+      return response.data[0].url;
+    } catch (err) {
+      console.log(err);
+      setError(String(err));
     } finally {
       setIsLoading(false);
     }
@@ -111,19 +146,38 @@ const Imagegen = ({ dGTranscript }: { dGTranscript: string | undefined }) => {
 
   return (
     <div className="w-1/2 flex flex-col justify-center items-center">
-      <button
-        className="btn btn-neutral ml-3"
-        onClick={generateImages}
-        disabled={isLoading || !isTransSet}
-      >
-        Generate Images
-      </button>
-      {isLoading && <p>Generating images...</p>}
-      <div className="w-auto p-4 grid grid-cols-2 gap-4">
-        {images.map((image, index) => (
-          <img key={index} src={image} alt={`Generated Image ${index}`} />
-        ))}
+      <div className="grid grid-cols-2 gap-3 p-4">
+        <button
+          className="btn btn-neutral ml-3"
+          onClick={generateImages}
+          disabled={isLoading || !isTransSet}
+        >
+          Generate Images Stability Ai
+        </button>
+        <button
+          className="btn btn-neutral ml-3"
+          onClick={generateDalleImages}
+          disabled={isLoading || !isTransSet}
+        >
+          Generate Images Dall-E
+        </button>
       </div>
+      {isLoading && <p>Generating images...</p>}
+      
+      { images.length>0 || openaimg.length>0?
+        <div className="w-auto p-4 grid grid-cols-2 gap-4">
+          {images.map((image, index) => (
+            <img key={index} src={image} alt={`Generated Image ${index}`} />
+          ))}
+        </div>:<div className=" text-red-400">{error}</div>
+      }
+      {openaimg.length>0|| images.length>0?
+        <div className="w-auto p-4 grid grid-cols-2 gap-4">
+          {openaimg.map((imageUrl, index) => (
+            <img key={index} src={imageUrl} alt={`Image ${index + 1}`} />
+          ))}
+        </div>:<div className=" text-red-400">{error}</div>
+      }
       {/* <div className="grid grid-cols-2 gap-4 p-4">
                 <img src="./download.png" alt="Burger" />
                 <img src="./download(1).png" alt="Burger" />
